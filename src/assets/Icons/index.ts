f6337f38ba3a47561ab551ef89b716f7662bae7e
Redo_7 <szczeposlaw668@gmail.com>
2025-08-08 14:02:01 -0400

    Add icon barell file

---
+++ src/assets/Icons/index.ts
@@ -0,0 +1,5 @@
+export { default as IconPlus } from './IconPlus';
+export { default as IconPlusSm } from './IconPlusSm';
+export { default as IconTrash } from './IconTrash';
+export { default as SidebarCollapse } from './SidebarCollapse';
+export { default as SidebarExpand } from './SidebarExpand';
