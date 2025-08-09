3f58d3b22b5cd0bcecd757c94d376d6617f93638
Redo_7 <szczeposlaw668@gmail.com>
2025-08-09 17:15:22 -0400

    Make ButtonField work

--- src/components/Fields/ButtonField.tsx
+++ src/components/Fields/ButtonField.tsx
@@ -3,13 +3,25 @@ import SubtleButton from '../Buttons/SubtleButton';
 interface Props {
 	name: string;
 	label: string;
-	value?: string;
 }
 
-const ButtonField = ({ name, label, value }: Props) => {
+const ButtonField = ({ name, label }: Props) => {
+	const handleButtonClick = () => {
+		
+		const iframe = (document?.querySelector("iframe") as HTMLIFrameElement) 
+		if (iframe?.contentWindow) {
+			iframe.contentWindow.postMessage(
+				{
+					listener: "onEventReceived",
+					detail: { event: { listener: "widget-button", field: name } }
+				},
+				"*"
+			);
+		}
+	}
 	return (
 		<div className="button-field">
-			<SubtleButton id={name} width="fit-content" padding=".25rem .75rem" height="2rem">
+			<SubtleButton id={name} onClick={handleButtonClick} width="fit-content" padding=".25rem .75rem" height="2rem">
 				{label}
 			</SubtleButton>
 		</div>
