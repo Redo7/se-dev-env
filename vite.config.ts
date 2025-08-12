139f5aa01be0416863febf8dd0432dbc5088b88f
Redo_7 <szczeposlaw668@gmail.com>
2025-08-12 19:19:38 -0400

    Add shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
+import path from "path"
 import react from '@vitejs/plugin-react';
 import { watchExtraFilesPlugin } from './vite-plugin-watch-extra';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), watchExtraFilesPlugin()],
+	plugins: [react(), watchExtraFilesPlugin(), tailwindcss()],
+	resolve: {
+		alias: {
+		  "@": path.resolve(__dirname, "./src"),
+		},
+	  },
 	server: {
 		port: 5173,
-
 		proxy: {
 			'/widgets': {
 				target: 'http://localhost:3001', // Your Node.js server
