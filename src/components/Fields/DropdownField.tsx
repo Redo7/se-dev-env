f61413bbe89bd7e47406fb871db68df5a79fa124
Redo_7 <szczeposlaw668@gmail.com>
2025-08-12 20:19:38 -0400

    Disable dropdown until an alternative is working

--- src/components/Fields/DropdownField.tsx
+++ src/components/Fields/DropdownField.tsx
@@ -1,4 +1,4 @@
-import ReactDropdown, { type Group, type Option } from 'react-dropdown';
+// import ReactDropdown, { type Group, type Option } from 'react-dropdown';
 import useFieldChange from '../../hooks/useFieldChange';
 import { useState } from 'react';
 
@@ -8,17 +8,17 @@ interface Props {
 	name: string;
 	label: string;
 	value: string;
-	options: (Group | Option | string)[] | Object;
+	options: Object;
 }
 
 const DropdownField = ({ overlay, widget, name, label, value, options }: Props) => {
 	const [inputValue, setInputValue] = useState(value);
-	const convertedOptions: Option[] = Object.entries(options).map(([objKey, val]) => ({
+	const convertedOptions = Object.entries(options).map(([objKey, val]) => ({
 		value: objKey,
 		label: val.toString(),
 	}));
 
-	const handleChange = (option: Option) => {
+	const handleChange = (option: {value: string}) => {
 		console.log(option.value)
 		setInputValue(option.value)
 		useFieldChange(overlay, widget, name, option.value);
@@ -27,7 +27,7 @@ const DropdownField = ({ overlay, widget, name, label, value, options }: Props) 
 	return (
 		<div className="dropdown-field">
 			<label>{label}</label>
-			<ReactDropdown name={name} value={inputValue} onChange={handleChange} options={convertedOptions} placeholder="Choose an option" />
+			{/* <ReactDropdown name={name} value={inputValue} onChange={handleChange} options={convertedOptions} placeholder="Choose an option" /> */}
 		</div>
 	);
 };
