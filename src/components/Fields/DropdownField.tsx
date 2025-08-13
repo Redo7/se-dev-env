7909ddde49e031364092ab1b88b933db2bbd9608
Redo_7 <szczeposlaw668@gmail.com>
2025-08-12 23:23:18 -0400

    Make dropdowns great again

--- src/components/Fields/DropdownField.tsx
+++ src/components/Fields/DropdownField.tsx
@@ -1,6 +1,12 @@
 // import ReactDropdown, { type Group, type Option } from 'react-dropdown';
 import useFieldChange from '../../hooks/useFieldChange';
 import { useState } from 'react';
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '../ui/command';
+import { Check, ChevronsUpDown } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { Button } from '../ui/button';
+import { Label } from '../ui/label';
 
 interface Props {
 	overlay: string;
@@ -13,22 +19,55 @@ interface Props {
 
 const DropdownField = ({ overlay, widget, name, label, value, options }: Props) => {
 	const [inputValue, setInputValue] = useState(value);
+	const [open, setOpen] = useState(false);
 	const convertedOptions = Object.entries(options).map(([objKey, val]) => ({
 		value: objKey,
 		label: val.toString(),
 	}));
 
-	const handleChange = (option: {value: string}) => {
-		console.log(option.value)
-		setInputValue(option.value)
-		useFieldChange(overlay, widget, name, option.value);
+	const handleChange = ( value: string ) => {
+		setInputValue(value);
+		useFieldChange(overlay, widget, name, value);
 	};
 
 	return (
-		<div className="dropdown-field">
-			<label>{label}</label>
-			{/* <ReactDropdown name={name} value={inputValue} onChange={handleChange} options={convertedOptions} placeholder="Choose an option" /> */}
-		</div>
+		<>
+		<Label>{label}</Label>
+		<Popover open={open} onOpenChange={setOpen}>
+			<PopoverTrigger asChild>
+				<Button variant="outline" role="combobox" aria-expanded={open} className="w-[200px] justify-between">
+					{inputValue
+						? convertedOptions.find((option) => option.value === inputValue)?.label
+						: 'Select framework...'}
+					<ChevronsUpDown className="opacity-50" />
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-[200px] p-0">
+				<Command>
+					<CommandInput placeholder="Search options..." className="h-9" />
+					<CommandList>
+						<CommandEmpty>No option found.</CommandEmpty>
+						<CommandGroup>
+							{convertedOptions.map((option) => (
+								<CommandItem
+									key={option.value}
+									value={option.value}
+									onSelect={(currentValue) => {
+										setOpen(false);
+										handleChange(currentValue)
+									}}>
+									{option.label}
+									<Check
+										className={cn('ml-auto', inputValue === option.value ? 'opacity-100' : 'opacity-0')}
+									/>
+								</CommandItem>
+							))}
+						</CommandGroup>
+					</CommandList>
+				</Command>
+			</PopoverContent>
+		</Popover>
+		</>
 	);
 };
 
