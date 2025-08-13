34db92b2d16439d0e0efd54b04f9eee488ad5270
Redo_7 <szczeposlaw668@gmail.com>
2025-08-12 20:19:03 -0400

    Fix styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Overlay from "./components/Overlay";
 import HomeScreen from "./components/HomeScreen";
+import { ThemeProvider } from './components/ThemeProvider';
 
 const App = () => {
 	// const [isDarkMode, setIsDarkMode] = useState(true);
-	const isDarkMode = true;
-	document.body.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
+	// const isDarkMode = true;
+	// document.body.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
 	return (
-		<Router>
-		  <Routes>
-			<Route path="/" element={<HomeScreen />} />
-			<Route path="/:id" element={<Overlay />} />
-		  </Routes>
-		</Router>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<Router>
+			<Routes>
+				<Route path="/" element={<HomeScreen />} />
+				<Route path="/:id" element={<Overlay />} />
+			</Routes>
+			</Router>
+		</ThemeProvider>
 	  );
 }
 
