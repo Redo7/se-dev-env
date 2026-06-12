fd866b54ea6feb3d5f853b2155785fec8a513064
Redo <szczeposlaw668@gmail.com>
2026-06-12 13:57:45 +0200

    chore: more compact chat design

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -49,7 +49,14 @@ const Chat = ({ closePopup }: Props) => {
   return (
     <div className="chat bg-zinc-900 rounded-lg border absolute w-70 overflow-hidden">
       <div className="flex justify-between items-center p-2 px-3">
-        <p className="text-[.75rem] tracking-wide font-[500]">Chat</p>
+        <div className="flex items-center text-[.75rem] tracking-wide font-[500]">
+          <p>Chat</p>
+          <p className="opacity-25 font-extralight ml-1">•</p>
+          <span className='flex items-center gap-1 py-1 px-2 pl-1 rounded-full transition-colors hover:bg-white/10'>
+            <img className="rounded-full size-4 border-2 box-border border-white/50" src="https://a.ppy.sh/2460045?1365236350.jpg" alt="Avatar" />
+            <p className="select-none">{username}</p>
+          </span>
+        </div>
         <Button variant="ghost" size="xs" onClick={closePopup}>
           <X size={16} />
         </Button>
@@ -67,8 +74,9 @@ const Chat = ({ closePopup }: Props) => {
           })}
         </div>
       </div>
-      <div className="bg-[#18181b] p-2 border-t">
-        <form action={handleChatMessage}>
+      <div className="bg-[#18181b] p-2 border-t flex gap-1 w-full items-center">
+        <Button variant="secondary"><CircleArrowOutUpRight className='size-3' /></Button>
+        <form action={handleChatMessage} className='w-full'>
           <InputGroup className="overflow-hidden h-fit break-word p-0!">
             <InputGroupTextarea
               ref={chatInput}
@@ -81,13 +89,6 @@ const Chat = ({ closePopup }: Props) => {
             </InputGroupAddon>
           </InputGroup>
         </form>
-        <div className="flex justify-between items-center mt-1.5">
-          <Button size="sm" variant="secondary" className="h-7 text-xs"><CircleArrowOutUpRight className='size-3' />∞</Button>
-          <span className='flex items-center gap-1 transition-colors hover:bg-white/10 p-1 rounded-md'>
-            <img className="rounded-full size-5 border-2 box-border border-white/50" src="https://a.ppy.sh/2460045?1365236350.jpg" alt="Avatar" />
-            <p className="text-xs font-medium tracking-wide select-none">{username}</p>
-          </span>
-        </div>
       </div>
     </div>
   );
